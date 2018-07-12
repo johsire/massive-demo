6ad8349c28a43f05f355c02aaae29940174489ee
Josiah Maina <josiah5115@gmail.com>
2018-07-12 11:01:16 -0600

    connecting db file-GET

--- server.js
+++ server.js
@@ -2,16 +2,24 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const massive = require('massive');
 
+require('dotenv').config();
+
+const connectionString = 
+
+
 const app = express();
 app.use(bodyParser.json());
 
 const port = 3000;
 
+// 'vipi' is a global variable for all my routes
 app.set('vipi', 'rasjoh');
 
 app.get('/', (req, res) => {
-  const mySetting = req.app.get('vipi')
-  res.send('massive-demo' + mySetting);
+  const db = req.app.get('db')
+  db.getAllInjuries().then(injuries => {
+    res.send(injuries);
+  })
 });
 
 app.get('/incidents', (req, res) => {
@@ -21,6 +29,10 @@ app.get('/incidents', (req, res) => {
 app.post('/incidents', (req, res) => {
   res.send({id: 123});
 });
+
+massive(connectionString).then(connection => {
+  app.set('db', connection);
+})
 
 app.listen(port, () => {
   console.log('Started server on port', port);
