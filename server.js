cd034f8e97715f014c047184f10a4dd1dfd3684e
Josiah Maina <josiah5115@gmail.com>
2018-07-12 11:26:25 -0600

    get-all-incidents

--- server.js
+++ server.js
@@ -23,7 +23,10 @@ app.get('/', (req, res) => {
 });
 
 app.get('/incidents', (req, res) => {
-  res.send([]);
+  const db = req.app.get('db');
+  db.getAllIncidents().then(incidents => {
+    res.send(incidents);
+  })
 });
 
 app.post('/incidents', (req, res) => {
