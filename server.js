bd48ae4e83700c57bac45f382b2efe0862afe42b
Josiah Maina <josiah5115@gmail.com>
2018-07-12 14:04:51 -0600

    sort incidents by state, injuryId & causeid

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const massive = require('massive');
-
-require('dotenv').config();
-
-const connectionString = 
+require('dotenv').config()
 
 
 const app = express();
+massive( process.env.CONNECTION_STRING ).then( dbInstance => app.set('db', dbInstance) );
 app.use(bodyParser.json());
 
-const port = 3000;
 
 // 'vipi' is a global variable for all my routes
 app.set('vipi', 'rasjoh');
@@ -40,12 +37,19 @@ app.get('/incidents', (req, res) => {
 });
 
 app.post('/incidents', (req, res) => {
-  res.send({id: 123});
+  const db = req.app.get('db');
+  const incident = req.body;
+  db.createIncident(incident).then(result  =>{
+  res.send(result);
+ });
 });
 
-massive(connectionString).then(connection => {
-  app.set('db', connection);
-})
+// massive(CONNECTION_STRING).then(connection => {
+//   app.set('db', connection);
+// })
+
+
+const port = 3000;
 
 app.listen(port, () => {
   console.log('Started server on port', port);
